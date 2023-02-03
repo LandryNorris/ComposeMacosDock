9279eff361fb5c071943d15929c341d07355a664
Landry Norris <landry.norris@live.com>
2023-02-02 22:01:25 -0600

    Minor visual changes

--- shared/src/commonMain/kotlin/CommonMain.kt
+++ shared/src/commonMain/kotlin/CommonMain.kt
@@ -1,5 +1,6 @@
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.LaunchedEffect
@@ -9,7 +10,9 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.ImageComposeScene
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
 import kotlinx.cinterop.addressOf
 import kotlinx.cinterop.usePinned
@@ -23,11 +26,13 @@ import platform.Foundation.dataWithBytes
 
 var counter by mutableStateOf(0)
 
+@Suppress("unused")
 fun commonMain() {
     NSApplication.sharedApplication()
 
     Window {
-        Column(modifier = Modifier.fillMaxSize(), verticalArrangement = Arrangement.Center,
+        Column(modifier = Modifier.fillMaxSize().background(Color.LightGray),
+            verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally) {
             Text("Counter is $counter")
             Row {
@@ -52,10 +57,13 @@ fun commonMain() {
 }
 
 fun createIcon(): NSImage {
-    val scene = ImageComposeScene(128, 128) {
-        Box(modifier = Modifier.fillMaxSize().background(Color.White),
+    val scene = ImageComposeScene(100, 100) {
+        Box(modifier = Modifier
+            .fillMaxSize()
+            .clip(RoundedCornerShape(20.dp))
+            .background(Color.White),
             contentAlignment = Alignment.Center) {
-            Column {
+            Column(horizontalAlignment = Alignment.CenterHorizontally) {
                 Text("Counter:")
                 Text(counter.toString())
             }
