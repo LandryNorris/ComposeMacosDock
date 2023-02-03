ae3c0b0ccbbdfe8b6cf1112cba9d170889b62bc8
Landry Norris <landry.norris@live.com>
2023-02-02 21:54:54 -0600

    Add counter

--- shared/src/commonMain/kotlin/CommonMain.kt
+++ shared/src/commonMain/kotlin/CommonMain.kt
@@ -1,14 +1,19 @@
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.ImageComposeScene
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.window.Window
 import kotlinx.cinterop.addressOf
 import kotlinx.cinterop.usePinned
+import kotlinx.coroutines.delay
 import platform.AppKit.NSApp
 import platform.AppKit.NSApplication
 import platform.AppKit.NSImage
@@ -16,10 +21,20 @@ import platform.AppKit.NSImageView
 import platform.Foundation.NSData
 import platform.Foundation.dataWithBytes
 
+var counter by mutableStateOf(0)
+
 fun commonMain() {
     NSApplication.sharedApplication()
 
     Window {
+        Column(modifier = Modifier.fillMaxSize(), verticalArrangement = Arrangement.Center) {
+            Text("Counter is $counter")
+            Row {
+                Button(onClick = { counter-- }) { Text("Decrement") }
+                Button(onClick = { counter++ }) { Text("Increment") }
+            }
+        }
+
         LaunchedEffect(Unit) {
             while(true) {
                 val imageView = NSImageView()
@@ -27,6 +42,7 @@ fun commonMain() {
 
                 NSApp?.dockTile?.setContentView(imageView)
                 NSApp?.dockTile?.display()
+                delay(1000)
             }
         }
     }
@@ -36,8 +52,12 @@ fun commonMain() {
 
 fun createIcon(): NSImage {
     val scene = ImageComposeScene(128, 128) {
-        Box(modifier = Modifier.fillMaxSize().background(Color.White)) {
-            Text("Test")
+        Box(modifier = Modifier.fillMaxSize().background(Color.White),
+            contentAlignment = Alignment.Center) {
+            Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                Text("Counter:")
+                Text(counter.toString())
+            }
         }
     }
 
